480158b1566345857f2b7755513b4bdf83e484f9
Dmitrii Dolbik <dda1508@gmail.com>
2023-01-02 14:34:41 +0300

    add solution

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -1,7 +1,21 @@
 package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
+    public static void main(String[] args) {
+        NumberToBreakOn numberToBreakOn = new NumberToBreakOn();
+        numberToBreakOn.printNumbersUntilWithBreakOn(4,8);
+    }
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        if (toBreakWith > numberToGoUntil){
+            System.out.println("iterating till the end");
+            for (int i = 0; i <= numberToGoUntil; i++) {
+                System.out.println(i);
+            }
+        } else {
+            for (int i = 0; i <= toBreakWith; i++) {
+                System.out.println(i);
+            }
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -1,7 +1,29 @@
 package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
+    public static void main(String[] args) {
+        TwoRangesSum twoRangesSum = new TwoRangesSum();
+        twoRangesSum.printSumOfTwoRanges(17,8);
+    }
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+        if (lastInRow < 0){
+            System.out.println("last number in row is negative");
+        } else if (numberToSkip > lastInRow){
+            System.out.println("number to skip is bugger then the last");
+        } else {
+            int skippedSum = 0;
+            int countedSum = 0;
+            for (int i = 1; i <= lastInRow; i++) {
+                if (i == numberToSkip){
+                    skippedSum += i;
+                    continue;
+                }
+                countedSum += i;
+
+            }
+            System.out.println("skipped sum is " + skippedSum);
+            System.out.println("counted sum is number " + countedSum);
+        }
 
     }
 }
