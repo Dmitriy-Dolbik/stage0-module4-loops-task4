c45dac1e51925d23153543362fb5a0012234a798
Dmitrii Dolbik <dda1508@gmail.com>
2023-01-08 12:25:59 +0300

    corrected solution

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -6,16 +6,15 @@ public class NumberToBreakOn {
         numberToBreakOn.printNumbersUntilWithBreakOn(4,8);
     }
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-        if (toBreakWith > numberToGoUntil){
+        if (toBreakWith > numberToGoUntil)
+        {
             System.out.println("iterating till the end");
+        }
             for (int i = 1; i <= numberToGoUntil; i++) {
                 System.out.println(i);
+                if (i == toBreakWith){
+                    break;
+                }
             }
-        } else {
-            for (int i = 1; i <= toBreakWith; i++) {
-                System.out.println(i);
-            }
-        }
-
     }
 }
