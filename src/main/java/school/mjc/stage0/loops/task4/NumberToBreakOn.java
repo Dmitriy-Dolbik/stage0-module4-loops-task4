1367c457a50bd06ed09489a8b44e0da770813901
Dmitrii Dolbik <dda1508@gmail.com>
2023-01-08 12:25:59 +0300

    corrected solution

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -3,13 +3,15 @@ package school.mjc.stage0.loops.task4;
 public class NumberToBreakOn {
     public static void main(String[] args) {
         NumberToBreakOn numberToBreakOn = new NumberToBreakOn();
-        numberToBreakOn.printNumbersUntilWithBreakOn(4,8);
+        numberToBreakOn.printNumbersUntilWithBreakOn(19,8);
     }
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
         if (toBreakWith > numberToGoUntil){
             System.out.println("iterating till the end");
             for (int i = 1; i <= numberToGoUntil; i++) {
                 System.out.println(i);
+            }
+            for (int i = 1; i <= numberToGoUntil; i++) {
             }
         } else {
             for (int i = 1; i <= toBreakWith; i++) {
